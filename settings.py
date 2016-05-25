944fccb707085280669d06faec3eac8d1f2daccc
JIA CUI <jacobcui123@gmail.com>
2016-05-25 21:13:52 +1000

    removing settings.py from .gitignore

---
+++ settings.py
@@ -0,0 +1,9 @@
+"""Application level variables."""
+
+import os
+APP_ROOT = os.path.dirname(os.path.realpath(__file__))
+TEMPLATE_DIR = os.path.join(APP_ROOT, 'templates')
+
+PROJECT_NAME = 'App Buffalo'
+
+
