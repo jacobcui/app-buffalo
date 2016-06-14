17f60a4bba64929079fa7af4b05f29b52fe23167
JIA CUI <jacobcui123@gmail.com>
2016-06-14 22:52:28 +1000

    Adding template for recaptcha.

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ import os
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE_DIR = os.path.join(APP_ROOT, 'templates')
 
-PROJECT_NAME = 'App Buffalo'
-
-
+PROJECT_NAME = 'Planet Snippets'
+reCAPTCHA_SITEKEY = '6LeEziETAAAAAGrNOmLBD1BEJPRKg3bjUTXV3qzo'
+reCAPTCHA_SECRET = '6LeEziETAAAAAFOfJ0l1r-HLokTfSMoh21bTXZJh'
